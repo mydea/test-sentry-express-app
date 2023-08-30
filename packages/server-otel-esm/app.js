f69e83e769dd83c208208439f8d00e0098adf8a8
Francesco Novy <francesco.novy@sentry.io>
2023-08-30 09:26:31 +0200

    remove requestHandler

--- packages/server-otel-esm/app.js
+++ packages/server-otel-esm/app.js
@@ -5,6 +5,5 @@ import Sentry from '@sentry/node-experimental';
 import {runApp} from '@test-express-app/core';
 
 runApp(Sentry, (app) => {
-  app.use(Sentry.Handlers.requestHandler());
   app.use(Sentry.Handlers.errorHandler());
 });
--- packages/server-otel/app.js
+++ packages/server-otel/app.js
@@ -5,6 +5,5 @@ const Sentry = require('@sentry/node-experimental');
 const { runApp } = require('@test-express-app/core');
 
 runApp(Sentry, (app) => {
-  app.use(Sentry.Handlers.requestHandler());
   app.use(Sentry.Handlers.errorHandler());
 });
